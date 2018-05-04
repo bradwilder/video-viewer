33b317a6c624a5a48e8489c9b1b07fe96132ae19
Brad <bradwilder31415@gmail.com>
2018-05-03 18:28:04 -0600

    Add input components (empty for now)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,29 @@ import { HttpModule } from '@angular/http';
 import { TableComponent } from './table/table/table.component';
 import { VideoService } from './video/video.service';
 import { TableFiltersComponent } from './table/table-filters/table-filters.component';
+import { ToggleSwitchComponent } from './inputs/toggle-switch/toggle-switch.component';
+import { ButtonComponent } from './inputs/button/button.component';
+import { CheckboxComponent } from './inputs/checkbox/checkbox.component';
+import { DropdownComponent } from './inputs/dropdown/dropdown.component';
+import { RadioComponent } from './inputs/radio/radio.component';
+import { StarRatingComponent } from './inputs/star-rating/star-rating.component';
+import { TextboxComponent } from './inputs/textbox/textbox.component';
+import { TimeInputComponent } from './inputs/time-input/time-input.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     TableComponent,
-    TableFiltersComponent
+    TableFiltersComponent,
+    ToggleSwitchComponent,
+    ButtonComponent,
+    CheckboxComponent,
+    DropdownComponent,
+    RadioComponent,
+    StarRatingComponent,
+    TextboxComponent,
+    TimeInputComponent
   ],
   imports: [
 	BrowserModule,

c06d6c6a5964f42d31d5eccb991d883865cecb69
Brad <bradwilder31415@gmail.com>
2018-05-22 16:28:13 -0600

    Time input improvements, including events

--- src/app/inputs/time-input/time-input.component.ts
+++ src/app/inputs/time-input/time-input.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, EventEmitter, Output } from '@angular/core';
 
 @Component
 ({
@@ -8,11 +8,53 @@ import { Component, Input } from '@angular/core';
 })
 export class TimeInputComponent
 {
+	@Input() value: {time: number, tol: number} = {time: 0, tol: 0}
 	@Input() enabled = true;
 	@Input() font = '';
+	@Output() changed = new EventEmitter<{time: number, tol: number}>();
+	private hours = 0;
+	private minutes = 0;
+	private seconds = 0;
+	private tol = 0;
 	
 	setEnabled(enabled: boolean)
 	{
 		this.enabled = enabled;
+		this.onChange();
+	}
+	
+	onHourChanged()
+	{
+		this.updateValue();
+		this.onChange();
+	}
+	
+	onMinuteChanged()
+	{
+		this.updateValue();
+		this.onChange();
+	}
+	
+	onSecondChanged()
+	{
+		this.updateValue();
+		this.onChange();
+	}
+	
+	onTolChanged()
+	{
+		this.updateValue();
+		this.onChange();
+	}
+	
+	updateValue()
+	{
+		this.value.time = this.hours * 3600 + this.minutes * 60 + this.seconds;
+		this.value.tol = this.tol;
+	}
+	
+	onChange()
+	{
+		this.changed.emit(this.value);
 	}
 }
